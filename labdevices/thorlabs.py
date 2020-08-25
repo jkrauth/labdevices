9af9b23cb376a08839ca77246e86d25024579f7d
Julian Krauth <mail@j-krauth.de>
2020-08-25 14:35:48 +0200

    should work now

--- labdevices/thorlabs.py
+++ labdevices/thorlabs.py
@@ -8,25 +8,24 @@ Python Version: 3.7
 import pyvisa
 from time import sleep
 
-"""
-In case that there is any problems with the initialization
-including error messages like 'Device or resource busy' the
-following might help to close the device first: 
-import usb.core
-device = usb.core.find(idVendor=4883, idProduct=33016)
-device.detach_kernel_driver(0)
-"""
+#In case that there is any problems with the initialization
+#including error messages like 'Device or resource busy' the
+#following might help to close the device first: 
+# import usb.core
+# device = usb.core.find(idVendor=4883, idProduct=33016)
+# device.detach_kernel_driver(0)
+
 
 class TSP01:
 
     DEFAULTS = {
         'read_termination': '\n',
-        'encoding': 'ascii',
+        'encoding':         'ascii',
     }
 
     device = None
 
-    def __init__(self, visa_addr):
+    def __init__(self, visa_addr: str):
         self.addr = visa_addr # e.g.: 'USB0::4883::33016::M00416750::0::INSTR'
         self.rm = pyvisa.ResourceManager()
         
@@ -41,26 +40,53 @@ class TSP01:
         sleep(0.5)
         print(f"Connected to {self.idn}.")
 
+    def close(self):
+        """Closes connection to device if open."""
+        if self.device is not None:
+            self.device.close()
+
     @property
-    def idn(self):
+    def idn(self) -> str:
         return self.device.query('*IDN?')
 
-    def temperature_usb(self):
+    def temperature_usb(self) -> float:
         """Returns built-in temperature of the TSP01 logger"""
         return self.device.query_ascii_values(':READ?')[0]
 
-    def humidity_usb(self):
+    def humidity_usb(self) -> float:
         """Returns built-in relative humidity"""
         return self.device.query_ascii_values(':SENSe2:HUMidity:DATA?')[0]
 
-    def temperature_probe1(self):
+    def temperature_probe1(self) -> float:
         """Returns temperature of external probe 1"""
         return self.device.query_ascii_values(':SENSe3:TEMPerature:DATA?')[0]
 
-    def temperature_probe2(self):
+    def temperature_probe2(self) -> float:
         """Returns temperature of external probe 2"""
         return self.device.query_ascii_values(':SENSe4:TEMPerature:DATA?')[0]
 
+
+class TSP01Dummy:
+    def __init__(self, _):
+        self.temp = 20
+        self.humidity = 50
+        self.idn = 'Thorlabs sensor dummy'
+
+    def initialize(self):
+        print('Connected to Thorlabs sensor dummy.')
+
     def close(self):
-        if self.device is not None:
-            self.device.close()
+        pass
+
+    def temperature_usb(self):
+        return self.temp
+
+    def humidity_usb(self):
+        return self.humidity
+
+    def temperature_probe1(self):
+        return self.temp
+
+    def temperature_probe2(self):
+        return self.temp + .5
+    
