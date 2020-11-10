fb742639691106002d51ab3a541973d8aaa5ab05
Julian Krauth <mail@j-krauth.de>
2020-11-10 13:26:26 +0100

    new module for devices from Rohde Schwarz. Started with the spectrum analyzer

---
+++ labdevices/rohde_schwarz.py
@@ -0,0 +1,45 @@
+"""
+Module for Rohde & Schwarz devices.
+"""
+
+import pyvisa
+import numpy as np
+from time import sleep
+
+class FPC1000:
+    """Simple spectrum analyzer."""
+    
+    def __init__(self, ip: str):
+        self.addr = 'TCPIP::'+ip
+        self.device = None
+        #self.timeout = 10000 # in ms, default is 2000
+        self.rm = pyvisa.ResourceManager()
+
+    def initialize(self):
+        self.device = self.rm.open_resource(self.addr)
+        self.device.timeout = self.timeout
+        print(f'Connected to {self.idn}')
+
+    def close(self):
+        if self.device is not None:
+            self.device.close()
+            print('Connection to FPC1000 closed!')
+        else:
+            print('FPC1000 is already closed.')
+        
+    def query(self, cmd: str):
+        respons = self.device.query(cmd).rstrip()
+        return respons
+
+    def get_trace(self):
+        raw_y = self.query('TRAC:DATA? TRACE1')
+        y = [float(i) for i in raw_y.split(',')]
+        sleep(0.1)
+        x_start = float(self.query('FREQ:STAR?'))
+        x_stop = float(self.query('FREQ:STOP?'))
+        x = list(np.linspace(x_start, x_stop, len(y)))
+        return x, y
+    
+    @property
+    def idn(self):
+        return self.query('*IDN?')
