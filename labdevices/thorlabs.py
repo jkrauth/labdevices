faaa6ffa0b7d363143056a0a939fbc31434d46d3
Julian Krauth <mail@j-krauth.de>
2021-02-06 21:03:08 +0100

    adapted some pylint stuff and other things

--- labdevices/thorlabs.py
+++ labdevices/thorlabs.py
@@ -1,23 +1,24 @@
 """
-Driver for Thorlabs temperature sensor TSP01.                               
+Module for Thorlabs devices.
 
 File name: thorlabs.py
+Author: Julian Krauth
 Date created: 2020/07/30
-Python Version: 3.7           
+Python Version: 3.7
 """
-import pyvisa
 from time import sleep
+import pyvisa
 
 #In case that there is any problems with the initialization
 #including error messages like 'Device or resource busy' the
-#following might help to close the device first: 
+#following might help to close the device first:
 # import usb.core
 # device = usb.core.find(idVendor=4883, idProduct=33016)
 # device.detach_kernel_driver(0)
 
 
 class TSP01:
-
+    """Driver for Thorlabs temperature sensor TSP01."""
     DEFAULTS = {
         'read_termination': '\n',
         'encoding':         'ascii',
@@ -28,14 +29,15 @@ class TSP01:
     def __init__(self, visa_addr: str):
         self.addr = visa_addr # e.g.: 'USB0::4883::33016::M00416750::0::INSTR'
         self.rm = pyvisa.ResourceManager()
-        
+
     def initialize(self):
+        """Connect to device."""
         self.device = self.rm.open_resource(
             self.addr,
             encoding=self.DEFAULTS['encoding'],
             read_termination=self.DEFAULTS['read_termination']
         )
-        
+
         # make sure connection is established before doing anything else
         sleep(0.5)
         print(f"Connected to {self.idn}.")
@@ -67,6 +69,7 @@ class TSP01:
 
 
 class TSP01Dummy:
+    """For testing purpose only. No device needed."""
     def __init__(self, _):
         self.temp = 20
         self.humidity = 50
@@ -89,4 +92,3 @@ class TSP01Dummy:
 
     def temperature_probe2(self):
         return self.temp + .5
-    
