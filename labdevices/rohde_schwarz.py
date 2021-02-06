37c4bad6848487df00a919128f1b34cec785132c
Julian Krauth <mail@j-krauth.de>
2021-02-06 20:56:15 +0100

    adapted some pylint stuff and other things

--- labdevices/rohde_schwarz.py
+++ labdevices/rohde_schwarz.py
@@ -1,17 +1,17 @@
 """
 Module for Rohde & Schwarz devices.
 """
-
-import pyvisa
-import numpy as np
 from time import sleep
+
+import numpy as np
+import pyvisa
 
 class FPC1000:
     """Simple spectrum analyzer.
     Works for now with an Ethernet connection.
     Bluetooth is not implemented.
     """
-    
+
     def __init__(self, ip: str):
         """Arguments:
         ip -- IP address of the device, e.g. '10.0.0.90'
@@ -30,7 +30,7 @@ class FPC1000:
     def idn(self) -> str:
         """Returns the identification string of the device."""
         return self.query('*IDN?')
-        
+
     def close(self):
         """Close connection to the device"""
         if self.device is not None:
@@ -38,7 +38,7 @@ class FPC1000:
             print('Connection to FPC1000 closed!')
         else:
             print('FPC1000 is already closed.')
-        
+
     def query(self, cmd: str) -> str:
         """Send a command and receive the answer"""
         respons = self.device.query(cmd).rstrip()
@@ -48,7 +48,7 @@ class FPC1000:
         """Get the trace which is currently shown on the display.
         For some reason this function sometimes times out.
         Increasing the timeout time couldn't solve the issue.
-        
+
         Return x and y as lists of floats.
         """
         raw_y = self.query('TRAC:DATA? TRACE1')
@@ -81,30 +81,31 @@ conn_types = ['usb', 'ethernet']
 
 class Oscilloscope:
     """
-    Is tested with the following Rohde & Schwarz oscilloscope 
+    Is tested with the following Rohde & Schwarz oscilloscope
     models:
-    
+
     """
     def __init__(self, instrument: str, connection_type: str):
         """
         Arguments:
         instrument -- str, Device Name from DICT
         connection_type -- str, 'USB' or 'ethernet'
-        """ 
+        """
         self.instrument = instrument
         self.device = None
         if connection_type == conn_types[0]:
             self.device_address = usb_dict[self.instrument]
         elif connection_type == conn_types[1]:
-            device_IPaddress = IP_dict[self.instrument]
-            self.device_address = (f'TCPIP::{device_IPaddress}::INSTR')
-    
+            device_ip_address = IP_dict[self.instrument]
+            self.device_address = (f'TCPIP::{device_ip_address}::INSTR')
+
     def initialize(self):
+        """Connect to device."""
         #rm_list = rm.list_resources()
-       
+
         self.device = rm.open_resource(self.device_address)
         print(f"Connected to:\n{self.idn}")
-       
+
 
     def query(self, cmd: str):
         response = self.device.query(cmd)
@@ -119,7 +120,7 @@ class Oscilloscope:
         response = self.device.query_binary_values(f'{cmd}', datatype='s')
 
         return response
-        
+
     @property
     def idn(self):
         idn = self.query("*IDN?")
@@ -130,11 +131,11 @@ class Oscilloscope:
         result = self.query("MEASurement:RESult?")
         return float(result)
 
-        
+
     def V_max(self,channel: int):
-        
+
         self.write(f"MEASurement:SOURce CH{channel}; MEASurement:MAIN UPEakvalue")
-        result = self.query("MEASurement:RESult?") 
+        result = self.query("MEASurement:RESult?")
         return float(result)
 
     def VPP(self, channel: int):
@@ -146,23 +147,21 @@ class Oscilloscope:
         print(f'acquiring trace for channel {channel+1}')
         self.write(f'CHANnel{channel}:SINGle')
         voltage = self.query(f'FORMat ASC; CHANnel{channel}:DATA?')
-        voltage = voltage.split(',')
-        for a, b in enumerate(voltage):
-            voltage[a] = float(b)
-     
+        voltage = [float(i) for i in voltage.split(',')]
 
-        x_header = self.query(f'CHANnel{channel}:DATA:HEADer?') # returns (xstart, xstop, length,Number of values per sample interval) as comma separated string
+        # this query returns (xstart, xstop, length,Number of values per sample interval) as string
+        x_header = self.query(f'CHANnel{channel}:DATA:HEADer?')
         x_header = x_header.split(',')
-        t = np.linspace(float(x_header[0]), float(x_header[1]), int(x_header[2]))
+        trace = np.linspace(float(x_header[0]), float(x_header[1]), int(x_header[2]))
 
-        return t, voltage
+        return trace, voltage
 
     def screen_shot(self):
-        # self.write('HCOPy:CWINdow ON') this closes all windows when taking screen shot so signal can be seen. 
-        # set format 
-        self.write('HCOPy:LANG PNG') 
+        # self.write('HCOPy:CWINdow ON') this closes all windows when taking screen shot so signal can be seen.
+        # set format
+        self.write('HCOPy:LANG PNG')
         image_bytes = self.ieee_query('HCOPy:DATA?')
-        
+
         return image_bytes
 
     def set_t_scale(self, time: str):
@@ -171,5 +170,5 @@ class Oscilloscope:
 
     def close(self):
         if self.device is not None:
-            self.device.before_close() 
+            self.device.before_close()
             self.device.close()
