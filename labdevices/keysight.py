fa5aab5af3b7685d9a34f9820add37b4d3debcc1
Julian Krauth <mail@j-krauth.de>
2021-02-06 20:41:06 +0100

    some pylint style modifications, not complete

--- labdevices/keysight.py
+++ labdevices/keysight.py
@@ -1,11 +1,9 @@
 """
-Model for keysight devices, like e.g. oscilloscopes.
+Module for keysight devices, like e.g. oscilloscopes.
 """
-
-import pyvisa as visa 
-import time
-import numpy as np
 from random import random
+import pyvisa as visa
+import numpy as np
 
 rm = visa.ResourceManager()
 # rm_list = rm.list_resources()
@@ -41,28 +39,29 @@ class Oscilloscope:
         if connection_type == conn_types[0]:
             self.device_address = usb_dict[instrument]
         elif connection_type == conn_types[1]:
-            device_IPaddress = IP_dict[instrument]
-            self.device_address = (f'TCPIP::{device_IPaddress}::INSTR')
+            device_ip_address = IP_dict[instrument]
+            self.device_address = (f'TCPIP::{device_ip_address}::INSTR')
 
     def initialize(self):
+        """Establish connection to device."""
         self.device = rm.open_resource(self.device_address)
 
         print(f"Connected to:\n{self.idn}")
 
-    def query(self,cmd):
+    def query(self, cmd):
         response = self.device.query(cmd)
         return response
 
-    def write(self,cmd):
+    def write(self, cmd):
         self.device.write(cmd)
 
-    def ieee_query(self,cmd):
+    def ieee_query(self, cmd):
         self.device.timeout = 20000
         self.write(cmd)
         response = self.device.query_binary_values(f'{cmd}', datatype='s')
         return response[0]
 
-        
+
     @property
     def idn(self):
         idn = self.query("*IDN?")
@@ -75,12 +74,12 @@ class Oscilloscope:
         self.write(f":MEASure:SOURce CHANnel{channel}")
         result = self.query(":MEASure:VAVerage?")
         return float(result)
-        
+
     def V_max(self, channel):
         self.write(f":MEASure:SOURce CHANnel{channel}")
         result = self.query(":MEASure:VMAX?")
         return float(result)
-        
+
 
     def VPP(self, channel):
         self.write(f":MEASure:SOURce {channel}")
@@ -88,14 +87,14 @@ class Oscilloscope:
         return float(result)
 
     def screen_shot(self):
-        """ 
+        """
         For some unknown reason, this function only works reliably
         when using ethernet connection to the oscilloscope
         """
         self.write(":HARDcopy:INKSaver OFF")
         image_bytes = self.ieee_query(":DISPlay:DATA? PNG, COLor")
         return image_bytes
-  
+
     def close(self):
         if self.device is not None:
             self.device.before_close()
@@ -105,11 +104,11 @@ class Oscilloscope:
 
         self.device.timeout = 10000
         self.write(':ACQuire:TYPE NORMal')
-        self.write(f':WAVeform:SOURce {channel}') 
+        self.write(f':WAVeform:SOURce {channel}')
         self.write(':WAVeform:POINts:MODE NORMal')
         self.write(':WAVeform:FORMat BYTE') #WORD or ASCii are other options
-        
-        time_scale = self.query(':TIMebase:SCALe?') 
+
+        time_scale = self.query(':TIMebase:SCALe?')
 
         (
         wav_form,
@@ -123,7 +122,7 @@ class Oscilloscope:
         y_origin,
         y_reference
         ) = self.query(":WAVeform:PREamble?").split(',')
-        
+
         # print(f'wave form is: {wav_form}')
         # print(f'acquire type is: {acq_type}')
         # print(f'wave form points are: {wfmpts}')
@@ -135,22 +134,22 @@ class Oscilloscope:
         # print(f'y reference is {y_reference}')
 
         data_bytes = self.ieee_query(":WAVeform:DATA?")
-    
-        nLength = len(data_bytes)        
-     
+
+        n_length = len(data_bytes)
+
         voltage_conversion = lambda x: (float(x) - float(y_reference)) * float(y_increment) + float(y_origin)
         voltage = [voltage_conversion(i) for i in data_bytes]
 
         x_min = eval(x_origin)
         step_size = eval(x_increment)
-        x_max = (x_min+(nLength*step_size))
+        x_max = (x_min+(n_length*step_size))
         time_val = np.arange(x_min, x_max, step_size)
         self.write(f':TIMebase:SCALe {time_scale}') #set time scale back to normal
         return time_val, voltage
-        
+
 
 class OscilloscopeDummy:
-
+    """Class for testing purpose only. No device needed."""
     def __init__(self, port):
         self.port = port
         self.result = 0
@@ -159,18 +158,18 @@ class OscilloscopeDummy:
         print("Connected to dummy device")
 
     def finalize(self):
-        print("Closing dummy device")   
-    
-    def V_avg(self,channel):
+        print("Closing dummy device")
+
+    def V_avg(self, channel):
         self.result = self.result + random()*10
         return self.result
 
-    def V_max(self,channel):
-        self.result = self.result + 1 
+    def V_max(self, channel):
+        self.result = self.result + 1
         return self.result
 
-    def VPP(self,channel):
-        self.VPP_result = random()* 10 
+    def VPP(self, channel):
+        self.VPP_result = random()* 10
         return self.VPP_result
 
     def set_t_scale(self, time):
@@ -187,7 +186,7 @@ class OscilloscopeDummy:
         y = self.result + random()*10
         print('dummy trace acquired')
         return x, y
-    
+
 
     def __str__(self):
         return "Dummy Driver"
